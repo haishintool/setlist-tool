8c19e0cdf514480d021b00d35a259ffd648b9849
haishintool <>
2026-08-06 09:35:37 +0900

    　不安　

--- script.js
+++ script.js
@@ -1461,10 +1461,45 @@ function renderDisplayPage() {
 
   stopAutoScroll();
 
-  if (setlistStrokeColorFilter) {
-  setlistStrokeColorFilter.setAttribute(
+if (setlistGlowColorFilter) {
+  setlistGlowColorFilter.setAttribute(
     "flood-color",
-    state.setlistStrokeColor
+    state.setlistShadowColor
+  );
+
+  if (setlistStrokeColorFilter) {
+    setlistStrokeColorFilter.setAttribute(
+      "flood-color",
+      state.setlistStrokeColor
+    );
+
+    setlistStrokeColorFilter.setAttribute(
+      "flood-opacity",
+      state.setlistStrokeEnabled
+        ? "1"
+        : "0"
+    );
+  }
+
+  if (setlistStrokeMorphology) {
+    setlistStrokeMorphology.setAttribute(
+      "radius",
+      String(state.setlistStrokeWidth)
+    );
+  }
+
+  setlistGlowColorFilter.setAttribute(
+    "flood-opacity",
+    state.setlistShadowEnabled
+      ? "1"
+      : "0"
+  );
+}
+
+if (setlistGlowBlur) {
+  setlistGlowBlur.setAttribute(
+    "stdDeviation",
+    String(state.setlistShadowBlur)
   );
 }
 
@@ -1624,11 +1659,11 @@ itemStroke.style.visibility =
 itemFill.style.color =
   state.setlistTextColor;
 
-itemFill.style.color =
-  state.setlistTextColor;
-
 itemFill.style.webkitTextStroke =
   "0px transparent";
+
+itemFill.style.filter =
+  'url("#setlistGlowFilter")';
 
 itemFill.style.filter =
   "url(#setlistGlowFilter)";  
